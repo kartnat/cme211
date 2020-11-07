84fad7105d3ff94a5b253929b062034ca91ff1ce
Karthik Nataraj <kartnat@stanford.edu>
2020-11-07 08:22:06 -0800

    interm

--- project/CGSolver.cpp
+++ project/CGSolver.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-#include "matvecops.hpp"
 #include "CGSolver.hpp"
+#include "matvecops.hpp"
 
 int CGSolver(std::vector<double> &val,
              std::vector<int>    &row_ptr,
@@ -10,80 +10,47 @@ int CGSolver(std::vector<double> &val,
              std::vector<double> &b,
              std::vector<double> &x,
              double              tol) {
-
-  double constant = -1;
-  std::vector<double> test = MatMult(val, row_ptr, col_idx, x);
-  //std::vector<double> test2(row_ptr.size()-1);
- // for (unsigned int n=0; n<test2.size(); n++)
-   // test2[n] = 1;
-  for (unsigned int n=0; n<test.size(); n++)
-        std::cout << "test " << test[n]<< std::endl;
-  std::vector<double> test2 = scalMult(constant, test);
-  for (unsigned int n=0; n<test2.size(); n++)
-      std::cout << "-test2 " << test2[n]<< std::endl;
-  for (unsigned int n=0; n<b.size(); n++)
-      std::cout << "b " << b[n]<< std::endl;
-  std::vector<double> r0 = vecAdd(b, test2);
+/*This function is a conjugate gradient solver.  It takes a matrix
+in CSR format with values, row pointer, and column vectors val,
+row_ptr, and col_idx, respectively, and solves the system Ax=b.  
+x is the starting guess and is modified in place until the desired
+tolerance is achieved.  The function returns the number of iterations 
+to get to that point.*/
   
-  //std::vector<double> a(row_ptr.size()-1);
-  for (unsigned int n=0; n<r0.size(); n++)
-    std::cout << "r0 " << r0[n]<< std::endl;
-  //std::cout << "r0 is " << r0 << std::endl;
-  //std::vector<double> r0 = vecAdd(b, scalMult(constant, MatMult(val, row_ptr, col_idx, x)));
+  std::vector<double> init = MatMult(val, row_ptr, col_idx, x);
+  init = scalMult(-1, init);
+  std::vector<double> r0 = vecAdd(b, init);
   double L2normr0 = L2norm(r0);
   std::vector<double> p_old = r0;
-  /*for (unsigned int n=0; n<p0.size(); n++)
-          std::cout << "p0 " << p0[n]<< std::endl;*/
   unsigned int niter = 0;
   std::vector<double> r_old = r0;
   
-  while (niter < row_ptr.size()) {
+  // Conjugate Gradient iterations
+  while (niter < row_ptr.size()-1) {
     niter = niter+1;
     std::vector<double> mat = MatMult(val, row_ptr, col_idx, p_old);
     double denom = dot(p_old, mat);
+    // alpha
     double alpha = (dot(r_old, r_old))/denom;
-    std::cout << "alpha " << alpha << std::endl; 
     std::vector<double> holder = scalMult(alpha, p_old);
-    //u_n+1
+    // u_n+1
     x = vecAdd(x, holder);
-    for (unsigned int n=0; n<x.size(); n++)
-        std::cout << "x " << x[n]<< std::endl;
-    //r_n+1
     std::vector<double> scal = scalMult(-alpha, mat);
     double betadenom = L2norm(r_old)*L2norm(r_old);
+    // r_n+1
     std::vector<double> r_new = vecAdd(r_old, scal);
     double L2normr = L2norm(r_new);
-    std::cout << "quotient: " << L2normr/L2normr0 << std::endl;
+    // Condition to break
     if (L2normr/L2normr0 < tol) {
-      break;
+      return niter;
     }
+    // beta_n
     double beta = (L2normr*L2normr)/betadenom;
     scal = scalMult(beta,p_old);
+    // p_n+1
     std::vector<double> p_new = vecAdd(r_new, scal);
     r_old = r_new;
     p_old = p_new;
   }
-  for(unsigned int n=0; n<x.size(); n++)
-    std::cout << "x[" << n << "] = " << x[n] << std::endl;
-  return niter;
-    
+  return -1;
 }
-/*
-int main() {
-  std::vector<double> val(4);
-  val[0] = 4;val[1] = 1; val[2] = 1; val[3] = 3;
-  std::vector<int> col_idx(4);
-  col_idx[0] = 0; col_idx[1] = 1; col_idx[2]=0; col_idx[3]=1;
-  std::vector<int> row_ptr(3);
-  row_ptr[0] = 0; row_ptr[1] = 2; row_ptr[2]=4;
-  std::vector<double> b(2);
-  b[0] = 0; b[1] = 0; 
-  std::vector<double> x(2);
-  x[0] = 1;x[1] = 1;
-  double tol = .00001;
-
-unsigned int niter = CGSolver(val, row_ptr, col_idx, b, x, tol);
-std::cout << "SUCCESS: CG solver converged in " << niter << " iterations" << std::endl;
-return 0;
-}
-*/
--- project/main.cpp
+++ project/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream> 
 #include <string>
 #include <vector>
@@ -6,56 +7,68 @@
 #include "COO2CSR.hpp"
 #include "CGSolver.hpp"
 
-int main(int, char *argv[]) {
+int main(int argc, char *argv[]) {
+/*This is the main function that invokes the conjugate gradient
+solver in CGSolver.cpp.  It loads a matrix in COO format, converts 
+it to CSR, solves the system and writes the solution vector to a
+solution file provided in the command line when running the executable */
+  
+  // Usage message if there are no command line arguments
+  if (argc < 2) {
+    std::cout << "Usage:" << std::endl;
+    std::cout << " " << argv[0] << " <input matrix file name> <output solution file name>" << std::endl; 
+    return 0;
+    
+  }
+  
+  // Reading in matrix in COO format
   std::string matFile = argv[1];
   std::ifstream f;
   f.open(matFile.c_str());
   unsigned int nrows, ncols;
   if (f.is_open()) {
-  
+    // Storing dimensions
     f >> nrows >> ncols;
     std::vector<int> col_idx;
     std::vector<int> row_idx;
     std::vector<double> val;
     int row, col;
     double value;
+    // row_idx: row indices; col_idx: column indices
+    // value: value at (row index, column index)
     while (f >> row >> col >> value) {
       row_idx.push_back(row);
       col_idx.push_back(col);
       val.push_back(value);
     }
     f.close();
+    // Converting to CSR format
     COO2CSR(val, row_idx, col_idx);
     
-    std::vector<double> b(50);
-    //std::cout << nrows << std::endl;
-    std::vector<double> x(50);
-    //b[0] = 0; b[1] = 0; x[0] = 1; x[1] = 1;
-    for (unsigned int n=0;n< 50;n++)
-      b[0] = 0; 
-      x[0] = 1;
-    //for (unsigned int n=0;n< nrows;n++)
-            // std::cout << x[n] << std::endl;
+    std::vector<double> b;
+    std::vector<double> x;
+    // Initializing target vector of zeroes
+    for (unsigned int n=0;n< nrows;n++)
+      b.push_back(0);
+    // Initializing first guess of x of ones
+    for (unsigned int n=0;n< nrows;n++) 
+       x.push_back(1);
+    x.resize(nrows);
+    b.resize(nrows);
+    // Initializing tolerance level and calling the solver
     double tol = .00001;
-    for (unsigned int n=0;n< row_idx.size();n++)
-             std::cout << "r " << row_idx[n] << std::endl;
-                 for (unsigned int n=0;n< col_idx.size();n++)
-                                   std::cout << "c " << col_idx[n] << std::endl;  
-                                       for (unsigned int n=0;n< val.size();n++)
-                                                std::cout << "v " << val[n] << std::endl;
-    unsigned int niter = CGSolver(val, row_idx, col_idx, b, x, tol);
+    int niter = CGSolver(val, row_idx, col_idx, b, x, tol);
     
-            
+    // Writing solution vector to solution file
+    std::string solnFile = argv[2];
+    std::ofstream g;
+    g.open(solnFile.c_str());
+    for (unsigned int n=0;n<nrows;n++)
+        g << std::fixed << std::setprecision(4) << std::scientific << x[n] << std::endl;
+    g.close();
+    
+    // Writing number of iterations to console
     std::cout << "SUCCESS: CG solver converged in " << niter << " iterations" << std::endl;
-    /*for (unsigned int n=0;n< row_idx.size();n++)
-         std::cout << row_idx[n] << std::endl;
-    for (unsigned int n=0;n< col_idx.size();n++)
-                  std::cout << col_idx[n] << std::endl;  
-    for (unsigned int n=0;n< val.size();n++)
-         std::cout << val[n] << std::endl;*/
-//    for (unsigned int n=0;n< row_idx.size();n++)
-  //      std::cout << row_idx[n] << std::endl; 
-  }
- 
+  } 
   return 0;
 }
